e6a5cf31f3d83c34c3866c4146536faafc24df8d
İşşa <Issamoha020@gmail.com>
2022-02-23 23:21:35 +0300

    Add files via upload

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -86,8 +86,8 @@ export default class Command extends BaseCommand {
       })();
       return {
         categories,
-        pack: pack[1] || "There you go ",
-        author: pack[2] || "LUMINE💃",
+        pack: pack[1] || "𝘔𝘺𝘵𝘩𝘪𝘤",
+        author: pack[2] || "İşşa☦",
         quality,
         type: StickerTypes[
           parsedArgs.flags.includes("--crop") ||
