3fce1e1b243b6cee1998c0b28819b5a09f45cd1b
İşşa <Issamoha020@gmail.com>
2022-02-23 23:01:13 +0300

    Update InviteLink.ts

--- src/commands/General/InviteLink.ts
+++ src/commands/General/InviteLink.ts
@@ -29,7 +29,7 @@ export default class Command extends BaseCommand {
         `*Invite link:* https://chat.whatsapp.com/${code}`,
         MessageType.text
       );
-      return void M.reply("Sent you the Group Link in personal message");
+      return void M.reply("Sent you the Group Link in personal message.📪");
     } else {
       return void M.reply(
         `Command not enabled by the admin.\nUse *${this.client.config.prefix}act invitelink* to enable it`
