1d9ad35286141d133fed10accf8f29dbb365476d
İşşa <Issamoha020@gmail.com>
2022-02-24 11:06:47 +0300

    Add files via upload

--- src/commands/Moderation/Demote.ts
+++ src/commands/Moderation/Demote.ts
@@ -1,36 +1,33 @@
-import MessageHandler from "../../Handlers/MessageHandler";
-import BaseCommand from "../../lib/BaseCommand";
-import WAClient from "../../lib/WAClient";
-import { ISimplifiedMessage } from "../../typings";
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
-  constructor(client: WAClient, handler: MessageHandler) {
-    super(client, handler, {
-      adminOnly: true,
-      command: "demote",
-      description: "demotes the mentioned users",
-      category: "moderation",
-      usage: `${client.config.prefix}demote [mention | @tag]`,
-      baseXp: 0,
-    });
-  }
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            adminOnly: true,
+            command: 'demote',
+            description: 'demotes the mentioned users',
+            category: 'moderation',
+            usage: `${client.config.prefix}demote [mention | @tag]`,
+            baseXp: 10
+        })
+    }
 
-  run = async (M: ISimplifiedMessage): Promise<void> => {
-    if (!M.groupMetadata?.admins?.includes(this.client.user.jid))
-      return void M.reply(`How can I demote someone without being an admin?`);
-    if (M.quoted?.sender) M.mentioned.push(M.quoted.sender);
-    if (!M.mentioned.length)
-      return void M.reply(`Tag the users you want to ${this.config.command}`);
-    M.mentioned.forEach(async (user) => {
-      const usr = this.client.contacts[user];
-      const username =
-        usr.notify || usr.vname || usr.name || user.split("@")[0];
-      if (!M.groupMetadata?.admins?.includes(user))
-        M.reply(`✖ Skipped *${username}* as they're not an admin`);
-      else if (user !== this.client.user.jid) {
-        await this.client.groupDemoteAdmin(M.from, [user]);
-        M.reply(`➰ Successfully Demoted *${username}*`);
-      }
-    });
-  };
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        if (!M.groupMetadata?.admins?.includes(this.client.user.jid))
+            return void M.reply(`❌ Failed to ${this.config.command} as I'm not an admin`)
+        if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
+        if (!M.mentioned.length) return void M.reply(`Please tag the users you want to ${this.config.command}`)
+        M.mentioned.forEach(async (user) => {
+            const usr = this.client.contacts[user]
+            const username = usr.notify || usr.vname || usr.name || user.split('@')[0]
+            if (!M.groupMetadata?.admins?.includes(user)) M.reply(`❌ Skipped *${username}* as they're not an admin`)
+            else if (user !== this.client.user.jid) {
+                await this.client.groupDemoteAdmin(M.from, [user])
+                M.reply(`*🚥Status:*\n\n✅Demoted *${username}*`)
+            }
+        })
+    }
 }
--- src/commands/Moderation/Purge.ts
+++ src/commands/Moderation/Purge.ts
@@ -1,54 +1,44 @@
-import MessageHandler from "../../Handlers/MessageHandler";
-import BaseCommand from "../../lib/BaseCommand";
-import WAClient from "../../lib/WAClient";
-import { ISimplifiedMessage } from "../../typings";
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
-  constructor(client: WAClient, handler: MessageHandler) {
-    super(client, handler, {
-      adminOnly: true,
-      command: "purge",
-      description: "Removes all group members",
-      category: "moderation",
-      usage: `${client.config.prefix}purge`,
-      baseXp: 0,
-    });
-  }
-
-  run = async (M: ISimplifiedMessage): Promise<void> => {
-    if (!(M.groupMetadata?.owner.split("@")[0] === M.sender.jid.split("@")[0]))
-      return void M.reply("Only the group owner can use this command");
-    if (!M.groupMetadata?.admins?.includes(this.client.user.jid))
-      return void M.reply(
-        "How can I remove all of the members without being an admin?"
-      );
-    if (!this.purgeSet.has(M.groupMetadata?.id || "")) {
-      this.addToPurge(M.groupMetadata?.id || "");
-      return void M.reply(
-        "Are you sure? This will remove everyone from the group chat. Use this command again if you'd like to proceed"
-      );
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            adminOnly: true,
+            command: 'purge',
+            description: 'Removes all group members',
+            category: 'moderation',
+            usage: `${client.config.prefix}purge`
+        })
     }
-    M.groupMetadata.participants.map(async (user) => {
-      if (!user.isAdmin)
-        await this.client
-          .groupRemove(M.from, [user.jid])
-          .catch(() => console.log("Failed to remove users"));
-    });
-    // now remove all admins except yourself and the owner
-    M.groupMetadata.admins.map(async (user) => {
-      if (user !== M.sender.jid && user !== this.client.user.jid)
-        await this.client
-          .groupRemove(M.from, [user])
-          .catch(() => console.log("error removing admin"));
-    });
-    await M.reply("Done!").catch(() => console.log("Failed to send message"));
-    this.client.groupLeave(M.from);
-  };
 
-  purgeSet = new Set<string>();
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        if (
+            M.groupMetadata?.owner !== M.sender.jid &&
+            M.groupMetadata?.owner !== M.sender.jid.replace('s.whatsapp.net', 'c.us')
+        )
+            M.reply('Only the group owner can use this command')
+        if (!M.groupMetadata?.admins?.includes(this.client.user.jid))
+            return void M.reply("I can't remove without being an admin")
+        if (!this.purgeSet.has(M.groupMetadata?.id || '')) {
+            this.addToPurge(M.groupMetadata?.id || '')
+            return void M.reply(
+                "Are you sure? This will remove everyone from the group chat. Use this command again if you'd like to proceed"
+            )
+        }
+        M.groupMetadata.participants.map(async (user) => {
+            if (!user.isAdmin) await this.client.groupRemove(M.from, [user.jid])
+        })
+        await M.reply('*🚥Status:* \n\n💣Purge Successful')
+        this.client.groupLeave(M.from)
+    }
 
-  addToPurge = async (id: string): Promise<void> => {
-    this.purgeSet.add(id);
-    setTimeout(() => this.purgeSet.delete(id), 60000);
-  };
+    purgeSet = new Set<string>()
+
+    addToPurge = async (id: string): Promise<void> => {
+        this.purgeSet.add(id)
+        setTimeout(() => this.purgeSet.delete(id), 60000)
+    }
 }
