bfa4365f8a069fd02b6b720318298ba4170ce0c0
İşşa <Issamoha020@gmail.com>
2022-02-24 11:42:13 +0300

    Update BroadCast.ts

--- src/commands/Dev/BroadCast.ts
+++ src/commands/Dev/BroadCast.ts
@@ -22,7 +22,7 @@ export default class Command extends BaseCommand {
         const term = joined.trim();
         const chats:any= this.client.chats.all().filter(v => !v.read_only && !v.archive).map(v => v.jid).map(jids => jids.includes("g.us")? jids : null).filter(v=>v);
         for(let i =0;i<chats.length;i++){
-        const text = `*「 Sapphire🈲 Broadcast 」* \n 📪𝗠𝗲𝘀𝘀𝗮𝗴𝗲: \n${term} \n 🪶𝗔𝘂𝘁𝗵𝗼𝗿: ${M.sender.username}`
+        const text = `*「 𝖠𝗌𝗎𝗇𝖺🈲 𝖡𝗋𝗈𝖺𝖽𝖼𝖺𝗌𝗍 」* \n 📪𝗠𝗲𝘀𝘀𝗮𝗴𝗲: \n${term} \n 🪶𝗔𝘂𝘁𝗵𝗼𝗿: ${M.sender.username}`
         this.client.sendMessage(chats[i], text,MessageType.text,{contextInfo : {mentionedJid : M.groupMetadata?.participants.map((user) => user.jid) }})
         }
     }
