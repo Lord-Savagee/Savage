3d798c3f585690acd49527bfe5184d94c2025373
İşşa <Issamoha020@gmail.com>
2022-02-24 10:13:38 +0300

    Update Bank.ts

--- src/commands/Economy/Bank.ts
+++ src/commands/Economy/Bank.ts
@@ -28,7 +28,7 @@ export default class Command extends BaseCommand {
 
     const buttonMessage: any = {
       contentText: `🏦 *Bank | ${M.sender.username}*\n\n🪙 *Gold: ${result}*`,
-      footerText: "©𝖠𝗌𝗎𝗇𝖺 2022",
+      footerText: "© 𝖠𝗌𝗎𝗇𝖺 2022",
       buttons: buttons,
       headerType: 1,
     };
