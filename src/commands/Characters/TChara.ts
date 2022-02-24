452f5e78bdf793eb921dab544e3ada9cf8e7ec48
İşşa <Issamoha020@gmail.com>
2022-02-24 10:13:12 +0300

    Update TChara.ts

--- src/commands/Characters/TChara.ts
+++ src/commands/Characters/TChara.ts
@@ -67,7 +67,7 @@ export default class Command extends BaseCommand {
     const text = `Character trade started\n\nOffer: ${gallery[i].name} (From ${gallery[i].source})\n\nFor: ${chara.name} (From ${source[0].anime.title})`;
     const buttonMessage: any = {
       contentText: `${text}`,
-      footerText: "©𝖠𝗌𝗎𝗇𝖺 2022",
+      footerText: "© 𝖠𝗌𝗎𝗇𝖺 2022",
       buttons: buttons,
       headerType: 1,
     };
