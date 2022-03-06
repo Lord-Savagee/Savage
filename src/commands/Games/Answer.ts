6b838688f24a12505112f4964ea0e765a0d08e3c
Lord Savage <100507699+smokey-vines@users.noreply.github.com>
2022-03-06 12:26:13 +0200

    Update Answer.ts

--- src/commands/Games/Answer.ts
+++ src/commands/Games/Answer.ts
@@ -18,7 +18,7 @@ export default class Command extends BaseCommand {
     M: ISimplifiedMessage,
     { joined }: IParsedArgs
   ): Promise<void> => {
-    if (M.from !== "120363022441013121@g.us")
+    if (M.from !== "263716564623-1628429288@g.us")
       return void M.reply(
         `You can't use this command here. Use ${this.client.config.prefix}support to get the quiz group link.`
       );
