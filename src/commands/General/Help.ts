1234c7acece7bd5a417e4354acb1d6f4d2d1d16a
Lord Savage <100507699+smokey-vines@users.noreply.github.com>
2022-03-06 14:41:14 +0200

    Create Help.ts

---
+++ src/commands/General/Help.ts
@@ -0,0 +1,70 @@
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import request from '../../lib/request'
+
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'help',
+            description: 'Displays the help menu or shows the info of the command provided',
+            category: 'general',
+            usage: `${client.config.prefix}help (command_name)`,
+            aliases: ['h', 'list', 'menu']
+        })
+    }
+
+     run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => {
+           const n = [
+           'https://www.linkpicture.com/q/20220103_202446.jpg'
+        ]
+        let chitoge = n[Math.floor(Math.random() * n.length)]
+        if (!parsedArgs.joined) {
+            const commands = this.handler.commands.keys()
+            const categories: { [key: string]: ICommand[] } = {}
+            for (const command of commands) {
+                const info = this.handler.commands.get(command)
+                if (!command) continue
+                if (!info?.config?.category || info.config.category === 'dev') continue
+                if (Object.keys(categories).includes(info.config.category)) categories[info.config.category].push(info)
+                else {
+                    categories[info.config.category] = []
+                    categories[info.config.category].push(info)
+                }
+            }
+            let text = `🍁 (༄ω༄) konichiws, 👋 *${M.sender.username}*\n\n•I am a *WhatsApp Bot* built to make your WhatsApp experience more fun.\n\n━━❰･Notes📝Side･❱━━\nMy name is *Elaina*.\n\nMy prefix is *${this.client.config.prefix}* and below are the usable commands 👇\n\n1`
+            const keys = Object.keys(categories)
+            for (const key of keys)
+                text += `╚━❰꧁Bot ${this.emojis[keys.indexOf(key)]} ${this.client.util.capitalize(key)}꧂❱━╝\n• \`\`\`${categories[
+                    key
+                ]
+                    .map((command) => command.config?.command)
+                     .join(', ')}\`\`\`\n\n`
+            return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.image, {
+
+
+            caption: `${text} 📝 *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*` }
+            )
+        }
+        const key = parsedArgs.joined.toLowerCase()
+        const command = this.handler.commands.get(key) || this.handler.aliases.get(key)
+        if (!command) return void M.reply(`No Command of Alias Found | "${key}"`)
+        const state = await this.client.DB.disabledcommands.findOne({ command: command.config.command })
+        M.reply(
+            `🚀 *Command:* ${this.client.util.capitalize(command.config?.command)}\n📉 *Status:* ${
+                state ? 'Disabled' : 'Available'
+            }\n⛩ *Category:* ${this.client.util.capitalize(command.config?.category || '')}${
+                command.config.aliases
+                    ? `\n♦️ *Aliases:* ${command.config.aliases.map(this.client.util.capitalize).join(', ')}`
+                    : ''
+            }\n🎐 *Group Only:* ${this.client.util.capitalize(
+                JSON.stringify(!command.config.dm ?? true)
+            )}\n💎 *Usage:* ${command.config?.usage || ''}\n\n📒 *Description:* ${command.config?.description || ''}`
+        )
+    }
+
+    emojis = ['', '', '','', '', '', '', '', '', '', '', '']
+}
