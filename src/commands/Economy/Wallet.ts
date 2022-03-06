0b22abad6196311e9bf48921307eb84ddf140b89
Kid-Hacker999 <100018711+Kid-Hacker999@users.noreply.github.com>
2022-03-06 07:08:10 +0200

    Update Wallet.ts

--- src/commands/Economy/Wallet.ts
+++ src/commands/Economy/Wallet.ts
@@ -28,7 +28,7 @@ export default class Command extends BaseCommand {
 
     const buttonMessage: any = {
       contentText: `👛 *Wallet | ${M.sender.username}*\n\n🪙 *Gold: ${result}*`,
-      footerText: "© 𝖠𝗌𝗎𝗇𝖺 2022",
+      footerText: "© Juice-Bot 2022",
       buttons: buttons,
       headerType: 1,
     };
