0334970723b5245268af53f9f8124b0977501134
Kid-Hacker999 <100018711+Kid-Hacker999@users.noreply.github.com>
2022-03-06 05:10:38 +0200

    Update Switch.ts

--- src/commands/Dev/Switch.ts
+++ src/commands/Dev/Switch.ts
@@ -26,7 +26,7 @@ export default class Command extends BaseCommand {
         { jid: M.from },
         { $set: { bot: "all" } }
       );
-      return void M.reply(`🟩 *Asuna🚀* is active now.`);
+      return void M.reply(`🟩 *Juice-Bot🧃* is active now.`);
     }
     if (i === bot) {
       if (this.client.user.name === i)
