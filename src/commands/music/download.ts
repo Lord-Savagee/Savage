2694bb2de2abba4a7037558d66119e6ff8d47dad
Lord Savage <100507699+smokey-vines@users.noreply.github.com>
2022-03-06 12:56:49 +0200

    Update and rename download to download.ts

--- src/commands/music/download.ts
+++ src/commands/music/download.ts
@@ -32,7 +32,7 @@ export default class Command extends BaseCommand {
       );
     const audio = new YT(videos[0].url, "audio");
     if (!audio.url) return;
-    M.reply("🚀downloading");
+    M.reply("🍁 sending");
     this.client
       .sendMessage(M.from, await audio.getBuffer(), MessageType.audio, {
         quoted: M.WAMessage,
