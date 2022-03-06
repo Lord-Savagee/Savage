1590933a2c36a20a86df62cce2338ab8c70b320e
Lord Savage <100507699+smokey-vines@users.noreply.github.com>
2022-03-06 12:11:29 +0200

    Update Deposit.ts

--- src/commands/Economy/Deposit.ts
+++ src/commands/Economy/Deposit.ts
@@ -56,7 +56,7 @@ export default class Command extends BaseCommand {
     await this.client.deposit(user, amount);
     const buttonMessage: any = {
       contentText: `🎉 You have transferred *${amount} gold* to your bank.`,
-      footerText: "© 𝖠𝗌𝗎𝗇𝖺 2022",
+      footerText: "© Elaina 2022",
       buttons: buttons,
       headerType: 1,
     };
