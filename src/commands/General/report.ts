09b4628aaf2edc9e4e38739d87dbeb20dbd0ad1a
Lord Savage <100507699+smokey-vines@users.noreply.github.com>
2022-03-12 11:37:59 +0200

    Create report.ts

---
+++ src/commands/General/report.ts
@@ -0,0 +1,34 @@
+import { MessageType } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from "../../typings"; 
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'rep',
+            aliases: ['report'],
+            description: 'Get the group invite link',
+            category: 'general',
+            usage: `${client.config.prefix}invite`,
+            baseXp: 10
+        })
+    }
+
+    run = async (
+
+		M: ISimplifiedMessage,		{ joined }: IParsedArgs
+
+	): Promise<void> => {
+        
+             const term = joined.trim()
+            await this.client.sendMessage(
+               // enter your unique gid
+`263716564623-1628429288@g.us`,
+                `「 BNH REPORT 」\n\n ${term} by ${M.sender.username} \n
+                   From : ${M.groupMetadata?.subject} `,
+                MessageType.text
+            );
+            return void M.reply('Sent the bot admin your report N.b if you used this command for fun you will be banned!!')
+        }}
