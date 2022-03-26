53dcd555c21abc3e668129b10054204c36cef853
Lord Savage <100507699+smokey-vines@users.noreply.github.com>
2022-03-26 10:58:06 +0200

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -28,7 +28,7 @@ export default class Command extends BaseCommand {
             for (const command of commands) {
                 const info = this.handler.commands.get(command)
                 if (!command) continue
-                if (!info?.config?.category || info.config.category === 'general') continue
+                if (!info?.config?.category || info.config.category === 'creation'|| info.config.category === 'general') continue
                 if (Object.keys(categories).includes(info.config.category)) categories[info.config.category].push(info)
                 else {
                     categories[info.config.category] = []
