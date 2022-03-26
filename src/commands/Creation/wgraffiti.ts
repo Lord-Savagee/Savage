5b57d6b3875660ea89dff7ae711bd267427477d2
Lord Savage <100507699+smokey-vines@users.noreply.github.com>
2022-03-26 08:37:45 +0200

    Update wgraffiti.ts

--- src/commands/Creation/wgraffiti.ts
+++ src/commands/Creation/wgraffiti.ts
@@ -35,7 +35,7 @@ export default class Command extends BaseCommand {
 					MessageType.image,
 					undefined,
 					undefined,
-					`…ᘛ⁐̤ᕐᐷ Here you go.\n`,
+					`🍁 Here you go.\n`,
 					undefined
 				).catch((e) => {
 					console.log(
