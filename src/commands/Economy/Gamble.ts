745584e01a032c85b13de426edb365879a7632e6
Toshiro  Hitsugaya <91844428+Toshiro223@users.noreply.github.com>
2022-03-28 07:40:45 +0530

    Update Gamble.ts

--- src/commands/Economy/Gamble.ts
+++ src/commands/Economy/Gamble.ts
@@ -91,7 +91,7 @@ export default class Command extends BaseCommand {
       await M.reply(await sticker.build(), MessageType.sticker, Mimetype.webp);
       const buttonMessage: any = {
         contentText: `📉 You lost *${amount} gold*.`,
-        footerText: "© Elaina 2022",
+        footerText: "© ZeroTwo 2022",
         buttons: buttons,
         headerType: 1,
       };
@@ -114,7 +114,7 @@ export default class Command extends BaseCommand {
       await M.reply(await sticker.build(), MessageType.sticker, Mimetype.webp);
       const buttonMessage: any = {
         contentText: `📈 You won *${amount} gold*.`,
-        footerText: "©Elaina 2022",
+        footerText: "©ZeroTwo 2022",
         buttons: buttons,
         headerType: 1,
       };
