1343bd302c964d3b21b70d98ed914c518c2bf6d5
Lord Savage <100507699+smokey-vines@users.noreply.github.com>
2022-03-29 15:44:22 +0200

    Update Bank.ts

--- src/commands/Economy/Bank.ts
+++ src/commands/Economy/Bank.ts
@@ -28,7 +28,7 @@ export default class Command extends BaseCommand {
 
     const buttonMessage: any = {
       contentText: `🏦 *Bank | ${M.sender.username}*\n\n🪙 *Gold: ${result}*`,
-      footerText: "© ZeroTwo 2022",
+      footerText: "© Elaina 2022",
       buttons: buttons,
       headerType: 1,
     };
