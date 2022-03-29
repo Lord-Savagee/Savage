9df6ecae3ed6a8fa7bb6cbed03320225b9432603
Lord Savage <100507699+smokey-vines@users.noreply.github.com>
2022-03-29 15:52:53 +0200

    Create Support.ts

---
+++ src/commands/General/Support.ts
@@ -0,0 +1,38 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'support',
+            aliases: ['support'],
+            description: 'Gets the support group links',
+            category: 'general',
+            usage: `${client.config.prefix}Support`,
+            baseXp: 10
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        (await this.client.sendMessage(
+        M.sender.jid,
+        `    ♥️MY MASTER GROUPS♥️
+        
+       *♥️ELAINA support:♥️* *https://chat.whatsapp.com/Gy1JemBGs9wLjVEfzbRJ21:*
+        *♥️support♥️* INVITE LINK🎉:*https://chat.whatsapp.com/KZEG60GfpiX1rPZTfe7nnW*`,
+           MessageType.text
+        ))
+        const n = [
+            './assets/videos/elain-git.mp4'
+        ]
+        let beckylynch = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url:beckylynch }, MessageType.video, {quoted:M.WAMessage,
+            mimetype: Mimetype.gif,
+            caption: `Sent you the support Link in personal message \n` }
+        )
+
+        }
+}
