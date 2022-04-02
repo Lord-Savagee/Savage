d6fb73b91601f66c235e4716030fffe4dc2637ed
union-savage <102780931+union-savage@users.noreply.github.com>
2022-04-02 08:29:26 +0200

    Update YTSearch.ts

--- src/commands/youtube/YTSearch.ts
+++ src/commands/youtube/YTSearch.ts
@@ -33,14 +33,14 @@ export default class Command extends BaseCommand {
         videos[i].author.name
       }\n 📙 *Duration:* ${videos[i].duration}\n📘 *URL:* ${videos[i].url}\n\n`;
     }
-    M.reply("Searching 🍁");
+    M.reply("🅢🅔🅐🅡🅒🅗🅘🅝🅖🍁");
     this.client
       .sendMessage(M.from, text, MessageType.extendedText, {
         quoted: M.WAMessage,
         contextInfo: {
           externalAdReply: {
             title: `Search Term: ${term}`,
-            body: `©Lord Savage edition`,
+            body: `©𝕃𝕠𝕣𝕕 𝕊𝕒𝕧𝕒𝕘𝕖 🕊️`,
             mediaType: 2,
             thumbnailUrl: videos[0].thumbnail,
             mediaUrl: videos[0].url,
