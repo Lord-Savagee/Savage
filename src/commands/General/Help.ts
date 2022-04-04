2fcd8eb77e3b3b6cab49cb90ba9588392594ee50
union-savage <102780931+union-savage@users.noreply.github.com>
2022-04-04 16:46:39 +0200

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -13,7 +13,7 @@ export default class Command extends BaseCommand {
             description: 'Displays the help menu or shows the info of the command provided',
             category: 'general',
             usage: `${client.config.prefix}help (command_name)`,
-            aliases: ['h']
+            aliases: ['h','menu']
         })
     }
 
