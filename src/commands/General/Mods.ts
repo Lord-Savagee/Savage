92c1fa7a6a84a996e04ec7f72d8a474f3ad9d6e9
Elainabot1/Lordsavage <102780931+union-savage@users.noreply.github.com>
2022-05-14 15:38:34 +0200

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -27,7 +27,9 @@ export default class Command extends BaseCommand {
           user.notify || user.vname || user.name || "null"
         }*\n🦋 *Contact: https://wa.me/+${user?.jid?.split("@")[0]}*\n\n`)
     );
-    text += `\n ❤️ Elaina 💙`;
+    text += `\n  ━━━━°❀•°:🤍 *Tokisaki Kurumi* 🤍:°•❀°━━━━
+
+©Aku 2022`;
     return void M.reply(text);
   };
 }
